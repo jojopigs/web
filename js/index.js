609738375d343502ad486ef06c9bf397d5ea028c
Hashirra <allan.kozaev@gmail.com>
2022-03-21 19:54:01 +0300

    rollback autizma dh

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 function selectSubject(evt, subjectName) {
     var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tab-content");
+    tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tab-links");
+    tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
@@ -14,4 +14,3 @@ function selectSubject(evt, subjectName) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
-
